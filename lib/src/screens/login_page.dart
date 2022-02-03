2420d938bdff1bf244b5934186ad8dfbb39b78c8
mati515 <mati8010100@gmail.com>
2022-02-03 23:47:41 +0500

    comments addedd

--- lib/src/screens/login_page.dart
+++ lib/src/screens/login_page.dart
@@ -12,7 +12,7 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     // bool _value = false;
-    // size.width * 1;
+    // size.width * 1;// from dev 
     return Scaffold(
       appBar: AppBar(),
       body: SingleChildScrollView(
